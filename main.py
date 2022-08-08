1cf4d56a7b6ac73eb6e4032988881b301e28123d
Carson 723 <23cadememy@gmail.com>
2022-08-08 16:38:25 -0500

    Added user input function for unwanted trip options on main.py

--- main.py
+++ main.py
@@ -13,27 +13,44 @@ def main_generator():
    displayed_greeting()
    while True:                                                                                                                     
         displayed_trip(generated_trip)
-        user_input = input('Does this trip suit you? if Yes type yes, if No type no')
+        user_input = input('Does this trip suit you? if Yes type yes, if No type no: ')
         if user_input == "yes":
             break
-        elif user_input == 'no':
+        elif user_input == "no":
             generated_trip = reselect_trip_option(generated_trip)
         else:
             print("That's not an option.")
+    
+   displayed_ending()
 
- 
-
+   displayed_trip(generated_trip)
 
 def displayed_greeting():
-    print('So I heard you need need a randomly generated trip? This is your randomly generated trip!')
+    print('So I heard you need need a randomly generated trip? Here is your randomly generated trip!')
 
 def displayed_ending():
-    print('Awesome! Glad help pick out your perfect trip')
+    print('Awesome! Glad to help pick out your perfect trip:')
 
 def displayed_trip(list_of_trip_options):
     trip_strings = ''
     trip_titles = ['Destination', 'Restaurant', 'Transportation', 'Entertainment']
     for index in range(len(list_of_trip_options)):
-        trip_strings += f' {trip_titles[index]} {list_of_trip_options[index]}'
+        trip_strings += f' {trip_titles[index]}: {list_of_trip_options[index]} \n'
 
-        print(trip_strings)
+    print(trip_strings)
+
+def reselect_trip_option(list_of_trip_options):
+    user_input = input('Which feature of the trip would you like to change? ')
+
+    if user_input == 'Destination':
+        list_of_trip_options[0] = random.choice(destinations_list)
+    elif user_input == 'Restaurant':
+        list_of_trip_options[1] = random.choice(restaurants_list)
+    elif user_input == 'Transportation':
+        list_of_trip_options[2] = random.choice(mode_of_transport_list)
+    elif user_input == 'Entertainment':
+        list_of_trip_options[3] = random.choice(entertainment_list)
+    
+    return list_of_trip_options
+
+main_generator()
